6e0fccaea480c5967c3a7a753430a51c09ef1a43
Gilson de lima <>
2024-08-26 13:25:40 -0300

    path variable exemplo

--- src/main/java/br/com/estudospring/funcionalidades/FuncionalidadesApplication.java
+++ src/main/java/br/com/estudospring/funcionalidades/FuncionalidadesApplication.java
@@ -1,14 +1,31 @@
 package br.com.estudospring.funcionalidades;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
+@RestController
 @SpringBootApplication
 public class FuncionalidadesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FuncionalidadesApplication.class, args);
+	}
+	
+	@GetMapping("/user/{id}")
+	public Map<String,Object> getUser(@PathVariable long id){
+		Map<String, Object> user = new HashMap<>();
+		user.put("id",id);
+		user.put("name","User: "+id);
+		user.put("email","user"+id+"@example.com");
+		return user;
 	}
 
 }
