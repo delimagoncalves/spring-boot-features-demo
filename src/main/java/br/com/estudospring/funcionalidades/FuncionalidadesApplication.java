41b5f3b3f8bcdef4d4dc7358a9fa524aae19f067
Gilson de lima <>
2024-08-26 13:20:06 -0300

    first commit

---
+++ src/main/java/br/com/estudospring/funcionalidades/FuncionalidadesApplication.java
@@ -0,0 +1,14 @@
+package br.com.estudospring.funcionalidades;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class FuncionalidadesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FuncionalidadesApplication.class, args);
+	}
+
+}
